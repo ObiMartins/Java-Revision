5c94074c0c9c8324ad7d8056ad42270fd2d2c292
Obi Martins <obichukwumartins@gmail.com>
2022-06-16 03:30:37 +0100

    First commit

---
+++ src/main/java/revision/Basic1.java
@@ -0,0 +1,13 @@
+package revision;
+
+public class Basic1 {
+
+    public String evenOddNumber(int num){
+        if(num % 2 == 0){
+            return num+" is an even number";
+        }
+        else{
+            return num+" is an odd number";
+        }
+    }
+}
---
+++ src/test/java/revision/Basic1Test.java
@@ -0,0 +1,13 @@
+package revision;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Basic1Test {
+
+    Basic1 bb = new Basic1();
+    void twoShouldReturnEven(){
+        assertEquals()
+
+    }
+
+}
