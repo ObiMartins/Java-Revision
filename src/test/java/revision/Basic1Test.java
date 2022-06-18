e3a0edd1a60867b24b2a36083f217471b293f6b4
Obi Martins <obichukwumartins@gmail.com>
2022-06-18 11:26:26 +0100

    commit with test class

--- src/test/java/revision/Basic1Test.java
+++ src/test/java/revision/Basic1Test.java
@@ -1,13 +1,25 @@
 package revision;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Basic1Test {
 
     Basic1 bb = new Basic1();
-    void twoShouldReturnEven(){
-        assertEquals();
+    @Test
+    public void twoMustReturnEven(){
+        assertEquals(2+" is an even number", bb.evenOddNumber(2));
+    }
 
+    @Test
+    public void threeMustReturnOdd(){
+        assertEquals(3+" is an odd number", bb.evenOddNumber(3));
+    }
+
+    @Test
+    public void EightyOneMustReturnOdd(){
+        assertEquals(81+" is an odd number", bb.evenOddNumber(81));
     }
 
 }
